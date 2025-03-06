0398eb732b4eaa7cc0fa282946467863adb7e6e9
vishaltechevangelist <37928313+vishaltechevangelist@users.noreply.github.com>
2025-03-06 23:50:19 +0530

    adding test files with non-existent payment

--- src/Order.php
+++ src/Order.php
@@ -2,5 +2,7 @@
 namespace UnitTestingApp;
 
 class Order {
-    
+    public function __construct(Payment $payment) {
+        $this->payment = $payment;
+    }
 }
--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -5,6 +5,10 @@ use UnitTestingApp\Order;
 
 class OrderTest extends TestCase {
     public function testOrderTranscation(){
-        $order = new Order;
+        //$payment_mock = $this->createMock(Payment::class); // willl throw an error for non-existent payment class
+        $payment_mock = $this->getMockBuilder(Payment::class)
+                            ->getMock(); // willl throw an erro
+        $order = new Order($payment_mock);
     }
+
 }
